d4670baab69ed29e426b172c6f15a746b95f50e8
mike <mike.ernst8002@gmail.com>
2026-04-16 11:40:43 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,22 @@
 import { getServerSession } from 'next-auth';
 import { fetchAllData } from '../lib/asana';
+import { CLIENTS, getClient, DEFAULT_CLIENT } from '../lib/clients';
 import Dashboard from '../components/Dashboard';
 
 export const revalidate = 172800;
 
-export default async function Home() {
+export default async function Home({ searchParams }) {
   const session = await getServerSession();
+
+  // Get client from URL param (?client=emirates) or default
+  const clientId = searchParams?.client || DEFAULT_CLIENT;
+  const client = getClient(clientId);
 
   let data = null;
   let error = null;
 
   try {
-    data = await fetchAllData();
+    data = await fetchAllData(client.projectGids);
   } catch (e) {
     error = e.message;
   }
@@ -22,6 +27,8 @@ export default async function Home() {
       error={error}
       userName={session?.user?.name || 'Team'}
       userImage={session?.user?.image}
+      clients={CLIENTS}
+      activeClient={client}
     />
   );
 }
